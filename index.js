18ea4e6e5349561aa794dd61b0b8ecb825bd48bd
i2R FrontMaker <i2r.frontmaker@gmail.com>
2020-04-11 16:56:31 +0600

    separate index.js create

---
+++ index.js
@@ -0,0 +1,27 @@
+Vue.directive('phone', {
+    bind(el) {  
+        el.oninput = function(e) {
+            if (!e.isTrusted) {
+            return;
+            }
+            let x = this.value.replace(/\D/g, '').match(/(\d{0,16})/);
+            // let x = this.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,4})/);
+            this.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+            el.dispatchEvent(new Event('input'));
+            }
+        }
+    }
+);
+
+new Vue({
+    el: "#app",
+    data: {
+        phone: ''
+    }
+})
+
+function openWA(){                
+    let number = document.getElementById("phone").value;                
+    window.open(`https://wa.me/${number}`)
+}
+        
